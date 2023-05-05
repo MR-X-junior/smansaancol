e0265086d4bfebf3d31d15a9fa8a86707c9028ad
Rahmat adha <68039885+MR-X-junior@users.noreply.github.com>
2023-05-05 12:00:51 +0800

    Add files via upload

---
+++ smansaancol/__version__.py
@@ -0,0 +1,8 @@
+__title__ = "smansaancol"
+__description__ = "Scraping Web Elearning SMA N 1 Muara Ancalong"
+__url__ = "https://github.com/MR-X-junior/smansaancol"
+__version__ = "0.0.1"
+__author__ = "Rahmat adha"
+__author_email__ = "rahmadadha11@gmail.com"
+__license__ = "MIT"
+__copyright__ = "Copyright Rahmat adha"
